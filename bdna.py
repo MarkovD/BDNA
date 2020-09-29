275982be7014181315312f8561b4ffda18a1206a
MarkovD <m.daurelio@hotmail.it>
2020-09-29 20:56:35 +0200

    morris algorithm

--- bdna.py
+++ bdna.py
@@ -1,6 +1,28 @@
+from random import randrange, randint, seed
+seed()
 
 def main():
-    pass
+    seq = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+    seq = [i for i in range(randint(0,1000001))]
+    print(len(seq))
+    oom = morris_algo(seq)
+    print(oom)
+
+def morris_algo(sequence):
+    
+    order_of_magnitude = 0
+
+    i=0
+    while i < len(sequence):
+        
+        random_int = randrange(10**order_of_magnitude)
+        
+        if random_int == 0:
+            order_of_magnitude += 1
+        
+        i+=1
+
+    return order_of_magnitude
 
 if __name__ == "__main__":
     main()
